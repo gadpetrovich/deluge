0113a9f70a91854715946c517e4c47fd74db0914
pavel <subber@yandex.ru>
2017-12-30 12:39:43 +0300

    убрал лишнее

--- start.sh
+++ start.sh
@@ -7,10 +7,6 @@ trap "echo TRAPed signal" HUP INT QUIT TERM
 #set -e
 UMASK=${UMASK:-002}
 
-#NAME1="deluged"
-#NAME2="deluge"
-#PIDFILE1=/var/run/$NAME1.pid
-#PIDFILE2=/var/run/$NAME2.pid
 DAEMON1=/usr/bin/deluged
 DAEMON2=/usr/bin/deluge-web
 DAEMON1_ARGS="-d -c /config -l /config/deluged.log"
@@ -18,18 +14,12 @@ DAEMON2_ARGS="-c /config -l /config/deluge-web.log"
 
 # Запуск
 echo "starting deluge"
-#umask "$UMASK"
 
-#adduser --home /var/lib/torrent --shell /usr/sbin/nologin --disabled-password -disabled-login --uid ${PUID} --gid ${PGID} --quiet torrent
 mkdir -p /var/lib/torrent
 groupadd --force --gid ${PGID} --non-unique torrent
 useradd --home-dir /var/lib/torrent --shell /usr/sbin/nologin --uid ${PUID} --gid ${PGID} --no-create-home --non-unique torrent
 
-#rm -f /config/deluged.pid
-
-#deluged -d -c /config -L info -l /config/deluged.log &
 start-stop-daemon --start --background --quiet --chuid $PUID:$PGID --user $PUID  --umask $UMASK --exec $DAEMON1 -- $DAEMON1_ARGS
-#deluge-web -c /config &
 start-stop-daemon --start --background --quiet --chuid $PUID:$PGID --user $PUID  --umask $UMASK --exec $DAEMON2 -- $DAEMON2_ARGS
 
   
@@ -39,9 +29,5 @@ read
 # Остановка
 echo "stopping deluge"
 
-#start-stop-daemon --stop --quiet --retry=TERM/30/KILL/5 --user $PUID --pidfile $PIDFILE2
-#start-stop-daemon --stop --quiet --retry=TERM/30/KILL/5 --user $PUID --pidfile $PIDFILE1
 
 echo "exited $0"
-
-#wait
