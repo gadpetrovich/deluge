f73cf82664ae790425aba16128f5d7ac7fae7dac
Pavel Sharapov <gadpetrovich@gmail.com>
2017-12-31 00:00:59 +0300

    перестало работать

--- Dockerfile
+++ Dockerfile
@@ -2,17 +2,16 @@ FROM ubuntu:16.04
 ARG BUILD_DATE
 ARG VERSION
 LABEL build_version="ubuntu: ${VERSION} Build-date: ${BUILD_DATE}"
-LABEL maintainer="gadpetrovich"
+LABEL maintainer="gadpetrovich <gadpetrovich@gmail.com>"
 
 RUN apt-get update -q && \
-    apt-get install -qy software-properties-common && \
+    apt-get install -qy net-tools software-properties-common && \
     add-apt-repository ppa:deluge-team/ppa && \
     apt-get update -q && \
-    apt-get install -qy deluged deluge-web
-
-#тестирование
-RUN apt-get --yes install net-tools
-
+    apt-get install -qy deluged deluge-web && \
+	apt-get clean && \
+	rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+	
 COPY start.sh /start.sh
 
 RUN chmod +x /start.sh
@@ -27,4 +26,5 @@ EXPOSE 8112
 # Daemon
 EXPOSE 58846
 
-CMD ["/start.sh"]
+#CMD ["/start.sh"]
+ENTRYPOINT ["/start.sh"]
--- start.sh
+++ start.sh
@@ -1,33 +1,31 @@
-#! /bin/bash
+#!/bin/bash
 
 # USE the trap if you need to also do manual cleanup after the service is stopped,
 #     or need to start multiple services in the one container
-trap "echo TRAPed signal" HUP INT QUIT TERM
+#trap "echo TRAPed signal" SIGTERM
 
 #set -e
-UMASK=${UMASK:-002}
+#UMASK=${UMASK:-002}
 
-DAEMON1=/usr/bin/deluged
-DAEMON2=/usr/bin/deluge-web
-DAEMON1_ARGS="-d -c /config -l /config/deluged.log"
-DAEMON2_ARGS="-c /config -l /config/deluge-web.log"
+#DAEMON1=/usr/bin/deluged
+#DAEMON2=/usr/bin/deluge-web
+#DAEMON1_ARGS="-d -c /config -l /config/deluged.log"
+#DAEMON2_ARGS="-c /config -l /config/deluge-web.log"
 
 # Запуск
-echo "starting deluge"
+#echo "starting deluge"
 
-mkdir -p /var/lib/torrent
-groupadd --force --gid ${PGID} --non-unique torrent
-useradd --home-dir /var/lib/torrent --shell /usr/sbin/nologin --uid ${PUID} --gid ${PGID} --no-create-home --non-unique torrent
+#mkdir -p /var/lib/torrent
+#groupadd --force --gid ${PGID} --non-unique torrent
+#useradd --home-dir /var/lib/torrent --shell /usr/sbin/nologin --uid ${PUID} --gid ${PGID} --no-create-home --non-unique torrent
 
-start-stop-daemon --start --background --quiet --chuid $PUID:$PGID --user $PUID  --umask $UMASK --exec $DAEMON1 -- $DAEMON1_ARGS
-start-stop-daemon --start --background --quiet --chuid $PUID:$PGID --user $PUID  --umask $UMASK --exec $DAEMON2 -- $DAEMON2_ARGS
+#start-stop-daemon --start --background --quiet --chuid $PUID:$PGID --user $PUID  --umask $UMASK --exec $DAEMON1 -- $DAEMON1_ARGS
+#start-stop-daemon --start --background --quiet --chuid $PUID:$PGID --user $PUID  --umask $UMASK --exec $DAEMON2 -- $DAEMON2_ARGS
 
-  
-echo "[hit enter key to exit] or run 'docker stop <container>'"
-read
+wait
 
 # Остановка
-echo "stopping deluge"
+#echo "stopping deluge"
 
 
-echo "exited $0"
+#echo "exited $0"
